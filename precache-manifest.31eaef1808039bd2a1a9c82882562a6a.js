a02ce2fc30267ca01405420381718ac5c53bbabd
Eduard <eduardstancu@yahoo.com>
2020-10-01 21:11:22 +0300

    Updates

---
+++ precache-manifest.31eaef1808039bd2a1a9c82882562a6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc0c929511eb52ecef703c60e77e1c6",
+    "url": "/anahita/index.html"
+  },
+  {
+    "revision": "b5d9ceb972d9f2ad7bef",
+    "url": "/anahita/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "57ccf52faa37f99961f1",
+    "url": "/anahita/static/css/main.4cf3ea16.chunk.css"
+  },
+  {
+    "revision": "b5d9ceb972d9f2ad7bef",
+    "url": "/anahita/static/js/2.bc51a8c0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/anahita/static/js/2.bc51a8c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57ccf52faa37f99961f1",
+    "url": "/anahita/static/js/main.f5d7bf6f.chunk.js"
+  },
+  {
+    "revision": "fe5325dabdda572c8565",
+    "url": "/anahita/static/js/runtime-main.68ca357f.js"
+  }
+]);
